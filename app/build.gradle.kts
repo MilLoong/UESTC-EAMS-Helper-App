0fc9ab4fe731bb32ef035cc13f22f0c805b78c94
Andue <14904096+andue@user.noreply.gitee.com>
2026-08-31 01:31:36 +0800

    Bump version to 1.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.milloong.uestc.Helper"
         minSdk = 24
         targetSdk = 36
-        versionCode = 33
-        versionName = "1.2.3"
+        versionCode = 34
+        versionName = "1.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
